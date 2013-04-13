f87e97f119a776b08fa9795654b26cef60373604
mamor <mamor.dev@gmail.com>
2013-04-13 20:58:07 +0900

    initial commit

---
+++ fuel/app/config/config.php
@@ -0,0 +1,29 @@
+<?php
+
+ini_set('default_charset', 'UTF-8');
+
+return array(
+	'language' => 'ja',
+	'locale' => 'ja_JP.utf8',
+	'default_timezone' => 'Asia/Tokyo',
+	'always_load' => array(
+		'packages' => array(
+			//'orm',
+			//'log',
+		),
+		'config' => array(
+		),
+	),
+
+	'security' => array(
+		'uri_filter' => array('htmlentities'),
+		'output_filter' => array('Security::htmlentities'),
+		'whitelisted_classes' => array(
+			'Fuel\\Core\\Response',
+			'Fuel\\Core\\View',
+			'Fuel\\Core\\ViewModel',
+			'Closure',
+		),
+	),
+
+);
