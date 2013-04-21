e39b4e8b60e913e278377169e94c0bc65c5ba153
mamor <mamor.dev@gmail.com>
2013-04-21 23:15:54 +0900

    removed appId

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -89,7 +89,7 @@ body { font-family: "<?php echo $webfont; ?>", sans-serif; }
 					var js, fjs = d.getElementsByTagName(s)[0];
 					if (d.getElementById(id)) return;
 					js = d.createElement(s); js.id = id;
-					js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=195197980550615";
+					js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
 					fjs.parentNode.insertBefore(js, fjs);
 				}(document, 'script', 'facebook-jssdk'));
 			</script>
