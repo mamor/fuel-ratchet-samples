4fbc043dd6f01a33e3bfb913311cced9ddc6056f
mamor <mamor.dev@gmail.com>
2013-05-26 13:30:58 +0900

    changed name "Single ChatRoom" to "Single ChatRoom with FuelPHP's Session class"

--- fuel/app/views/index/index.php
+++ fuel/app/views/index/index.php
@@ -5,14 +5,14 @@
 <table class="table">
 	<thead>
 		<tr>
-			<th style="width: 25%;">Name</th>
-			<th style="width: 25%;">Type</th>
+			<th style="width: 30%;">Name</th>
+			<th style="width: 20%;">Type</th>
 			<th style="width: 50%;">Url</th>
 		</tr>
 	</thead>
 	<tbody>
 		<tr>
-			<td>Single ChatRoom</td>
+			<td>Single ChatRoom with FuelPHP's Session class</td>
 			<td>WsServer</td>
 			<td><?php echo Html::anchor('ratchet/ws/chat/join'); ?></td>
 		</tr>
